95703257cea35c07c2669e498ab966c0e5b02c59
aid402 <aid402@gmail.com>
2017-07-31 15:55:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import machine,os,sys,network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    aplist = sta_if.scan()
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect('LP_wireles', '1871157210')
+    while not sta_if.isconnected():
+        pass
+print('network config:', sta_if.ifconfig())
