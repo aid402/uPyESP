781d715a29aed85d10354adb6c53a42f04d964d7
aid402 <aid402@gmail.com>
2017-08-01 08:58:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import machine,os,sys,network
 sta_if = network.WLAN(network.STA_IF)
+ap_stock = [('LP_wireles','1871157210')]
 if not sta_if.isconnected():
     aplist = sta_if.scan()
-    print('connecting to network...')
-    sta_if.active(True)
-    sta_if.connect('LP_wireles', '1871157210')
-    while not sta_if.isconnected():
-        pass
-print('network config:', sta_if.ifconfig())
+    for i in aplist:
+        ap = str(i[0].'utf-8')
+        for p in ap_stock:
+            if ap == ap_stock[p][0]:
+                print('connecting to network...')
+                sta_if.active(True)
+                sta_if.connect(ap_stock[p][0], ap_stock[p][1])
+                while not sta_if.isconnected():
+                    pass
+                break
+        if sta_if.isconnected():
+            print('network config:', sta_if.ifconfig())
+            break
+
