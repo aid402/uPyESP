5eae8a7724efc2a228e4703575790cfd27015387
aid402 <aid402@gmail.com>
2017-08-23 21:35:36 +0700

    Update onebutton.py

--- onebutton.py
+++ onebutton.py
@@ -2,22 +2,34 @@ from machine import Pin
 
 class Config:
         
-    def __init__(self, box_id, pinButton, pinSwitch, patternList):
+    def __init__(self, box_id, pinSwitch, patternList):
         self.state = 0
         self.box_id = box_id
-        self.Button = Pin(pinButton, Pin.IN, Pin.PULL_UP)
-        self.
+        self.pinSwitch = pinSwitch
+        self.numSwitch = len(pinSwitch)
         self.patternList = patternList
-        
-        
-        for i in range(len(pinRelay)):
-            self.relay[i] = Pin(pinRelay[i], Pin.OUT, value=0)
+        self.switch_0 = pinSwitch[0]
+        if self.numSwitch > 1:
+            self.switch_1 = Pin(pinSwitch[1], Pin.OUT, value=0)
+        if self.numSwitch > 2:
+            self.switch_2 = Pin(pinSwitch[2], Pin.OUT, value=0)
+        if self.numSwitch > 3:
+            self.switch_3 = Pin(pinSwitch[3], Pin.OUT, value=0)
+        #...
+        #...
         
     def turn(self, pattern_num):
-        p = self.Pattern[pattern_num]
-        for n in range(len(self.relay)):
-            self.relay[n].value(int(p[n]))
-    
+        p = self.patternList[pattern_num]
+        self.switch_0.value(int(p[0]))
+        if self.numSwitch > 1:
+            self.switch_1.value(int(p[1]))
+        if self.numSwitch > 2:
+            self.switch_2.value(int(p[2]))
+        if self.numSwitch > 3:
+            self.switch_3.value(int(p[3]))
+        #...
+        #...
+
     def push(self):
         self.state += 1
         if self.state >= len(self.relay) :
