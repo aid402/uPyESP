e6b121e67282958f4d8b7d13ce166a1c0c8958cf
aid402 <aid402@gmail.com>
2017-08-23 15:07:21 +0700

    Update onebutton.py

--- onebutton.py
+++ onebutton.py
@@ -1,15 +1,21 @@
 from machine import Pin
-import time
 
 class RELAYConfig:
         
-    def __init__(relay_id, pinButton, pinRelay, patturn):
-        button = Pin(pinButton, Pin.IN, Pin.PULL_UP)
+    def __init__(self, relaySet_id, pinButton, pinRelay, Pattern):
+        self.state = 0
+        self.relaySet_id = relaySet_id
+        self.Pattern = Pattern
+        self.Button = Pin(pinButton, Pin.IN, Pin.PULL_UP)
         for i in range(len(pinRelay)):
-            relay[i] = Pin(pinRalay[i], Pin.OUT, value=0)
-
-stat=0
-while True:
-    if not button1.value():
-        stat++
-        if stat=4:
+            self.relay[i] = Pin(pinRelay[i], Pin.OUT, value=0)
+        
+    def turn(self, pattern_num):
+        p = self.Pattern[pattern_num]
+        for n in range(len(self.relay)):
+            self.relay[n].value(int(p[n]))
+    
+    def push(self):
+        self.state += 1
+        if self.state >= len(self.relay) :
+            self.state = 0
