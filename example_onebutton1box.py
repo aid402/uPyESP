3605cdc9cef3cef9bc67d6f25bc001201e459d00
aid402 <aid402@gmail.com>
2017-08-23 22:59:45 +0700

    Create example_onebutton1box.py

---
+++ example_onebutton1box.py
@@ -0,0 +1,21 @@
+from machine import Pin
+from time import sleep
+import onebutton
+
+#switch box @living room
+boxID_1 = 'B1' 
+pinList_1 = [14,12,13]
+patList_1 = ['000','100','110','111','001']
+
+#create onebutton object
+Box_01 = onebutton.Config(boxID_1, pinList_1, patList_1)
+
+#set pin 15 as button@living room
+button1 = Pin(15, Pin.IN, Pin.PULL_UP)
+
+while True:
+    while not button1.value():
+        pass
+    pattern = Box_01.push()
+    Box_01.turn(pattern)
+    time.sleep_ms(300)
