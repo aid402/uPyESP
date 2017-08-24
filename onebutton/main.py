15757765c1eb00d70d50fc962b3fb4b463860057
aid402 <aid402@gmail.com>
2017-08-24 23:58:39 +0700

    Update main.py

--- onebutton/main.py
+++ onebutton/main.py
@@ -31,18 +31,38 @@ CONFIG = {
 }
 Topic1=b"box1/pattern"
 
+#switch box @living room
+boxID_1 = 'B1' 
+pinList_1 = [14,12,13]
+patList_1 = ['000','100','110','111','001']
+
+#create onebutton object
+box = onebutton.Config(boxID_1, pinList_1, patList_1)
+
+#set pin 15 as button
+button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
+
 def sub_cb(topic, msg):
     if msg != box.patternList[box.state]:
+        led.off()
         box.turn(msg)
+        led.on()
         
 def main():
     client = MQTTClient(CONFIG['CLIENT_ID'], CONFIG['MQTT_BROKER'], user=CONFIG['USER'], password=CONFIG['PASSWORD'], port=CONFIG['PORT'])
     client.set_callback(sub_cb)
     client.connect()
     client.subscribe(Topic1)
+    client.check_msg()
+    msg = json.dumps({
+        'IP':ip_node,
+        'pattern':box.patternList[box.push()],
+        })
+    client.publish(Topic1,msg)
     while True:
         client.check_msg()
         if button.value():
+            led.off()
             msg = json.dumps({
                 'IP':ip_node,
                 'pattern':box.patternList[box.push()],
@@ -50,5 +70,6 @@ def main():
             client.publish(Topic1,msg)
             box.turn(box.patternList[box.state])
             sleep_ms(300)
+            led.on()
 
 main()
