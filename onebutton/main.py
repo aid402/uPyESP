47621c617d2c660f9636c90311fd187cf494e7bc
aid402 <aid402@gmail.com>
2017-08-26 17:18:46 +0700

    Add files via upload

--- onebutton/main.py
+++ onebutton/main.py
@@ -1,62 +1,11 @@
-from umqtt.simple import MQTTClient
-from time import sleep_ms
-import machine,json,dht,ubinascii,onebutton,wifi
-
-# Modify below section as required
-CONFIG = {
-     # Configuration details of the MQTT broker
-     "MQTT_BROKER": "192.168.1.46",
-     "USER": "",
-     "PASSWORD": "",
-     "PORT": 1883,
-     # unique identifier of the chip
-     "CLIENT_ID": b"ESP8266" + ubinascii.hexlify(machine.unique_id())
-}
-Topic = b"box1/pattern"
-Topic1 = b"box1/msg"
-ip_node = wifi.connect()
-
-#switch box @living room
-boxID = 'B1' 
-pinList = [14,12,13]
-patList = ['000','100','110','111','001']
-
-#create onebutton object
-box = onebutton.Config(boxID, pinList, patList)
-
-#set pin 15 as button
-button = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)
-
-def sub_cb(topic, msg):
-    if msg != box.pattern:
-        led.off()
-        box.pattern = msg
+from umqtt.simple import MQTTClientfrom time import sleep_msimport networkimport machine,json,dht,ubinascii,onebuttonled = machine.Pin(2,machine.Pin.OUT,value=0)#connect wifista_if = network.WLAN(network.STA_IF)if not sta_if.isconnected():    sta_if.active(True)    sta_if.connect('LP_wireless', '1871157210')    while not sta_if.isconnected():        pass    node_ip = sta_if.ifconfig()[0]    led.value(1)else:    node_ip = sta_if.ifconfig()[0]    led.value(1)# Modify below section as requiredCONFIG = {     # Configuration details of the MQTT broker     "MQTT_BROKER": "192.168.1.46",     "USER": "",     "PASSWORD": "",     "PORT": 1883,     # unique identifier of the chip     "CLIENT_ID": b"ESP8266" + ubinascii.hexlify(machine.unique_id())}Topic = b"box1/pattern"#switch box @living roomboxID = 'B1' pinList = [14,12,13]patList = ['000','100','110','111','001']#create onebutton objectbox = onebutton.Config(boxID, pinList, patList)#set pin 15 as buttonbutton = machine.Pin(15, machine.Pin.IN, machine.Pin.PULL_UP)def sub_cb(topic, msg):
+    msg = json.loads(msg)
+    print(msg)
+    if msg['pattern'] != box.pattern:        led.off()
+        box.pattern = msg['pattern']
         box.iturn()
-        led.on()
-        
-def main():
-
-    client = MQTTClient(CONFIG['CLIENT_ID'], CONFIG['MQTT_BROKER'], user=CONFIG['USER'], password=CONFIG['PASSWORD'], port=CONFIG['PORT'])
-    client.set_callback(sub_cb)
-    client.connect()
-    client.subscribe(Topic1)
-    client.check_msg()
-    msg = json.dumps({
-        'IP':ip_node,
-        'pattern':box.patternList[box.push()],
-        })
-    client.publish(Topic,msg)
-    while True:
-        client.check_msg()
-        if button.value():
-            led.off()
-            msg = json.dumps({
-                'IP':ip_node,
-                'pattern':box.patternList[box.push()],
-                })
-            client.publish(Topic,msg)
-            box.turn(box.patternList[box.state])
-            sleep_ms(300)
-            led.on()
-
-main()
+        if box.pattern in box.patternList:
+            box.state = box.patternList.index(box.pattern)        led.on()#MQTT connectclient = MQTTClient(CONFIG['CLIENT_ID'], CONFIG['MQTT_BROKER'], user=CONFIG['USER'], password=CONFIG['PASSWORD'], port=CONFIG['PORT'])client.set_callback(sub_cb)client.connect()client.subscribe(Topic)        def main():    msg = json.dumps({        'IP':node_ip,        'pattern':box.patternList[0],        })    client.publish(Topic,msg)    while True:
+        while not button.value():
+            client.check_msg()        if button.value():            led.off()            msg = json.dumps({                'IP':node_ip,                'pattern':box.patternList[box.push()],                })            client.publish(Topic,msg)            box.turn()            sleep_ms(300)            led.on()
+main()
