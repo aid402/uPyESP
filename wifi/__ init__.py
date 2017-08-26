2dc902bc036d734bb27e7f74e1abc4f858d97c34
aid402 <aid402@gmail.com>
2017-08-26 21:33:18 +0700

    Update __ init__.py

--- wifi/__ init__.py
+++ wifi/__ init__.py
@@ -10,7 +10,6 @@ def connect():
             ap_i = bytes.decode(i[0])
             for p in ap_stock:
                 if ap_i == p[0]:
-                    print('connecting to network...')
                     sta_if.active(True)
                     sta_if.connect(p[0], p[1])
                     while not sta_if.isconnected():
