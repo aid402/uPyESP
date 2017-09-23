5a3b74ab26c5996cb47cc9bfc9f343f7210ee8e0
aid402 <aid402@gmail.com>
2017-09-23 17:25:43 +0700

    Create smartconfig.py

---
+++ wifi/smartconfig.py
@@ -0,0 +1,33 @@
+def smartconfig():
+    html = """<!DOCTYPE html>
+    <html>
+        <head> <title>ESP8266 Smart Config</title> </head>
+        <body> <h1>ESP8266 Smart Config</h1>
+                <table border="1"> <tr><th>SSID</th><th>Value</th></tr> %s </table>
+        </body>
+    </html>
+    """
+
+    import socket
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+    sta_if = network.WLAN(network.STA_IF)
+    ap_scan = sta_if.scan()
+    
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (bytes.decode(i[0]), bytes.decode(i[1])) for i in ap_scan]
+        response = html % '\n'.join(rows)
+        cl.send(response)
+        cl.close()
