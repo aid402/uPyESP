5e43412e251c717969440a395613626d54054af7
aid402 <aid402@gmail.com>
2017-09-23 21:57:20 +0700

    Update smartconfig.py

--- wifi/smartconfig.py
+++ wifi/smartconfig.py
@@ -3,7 +3,7 @@ def smartconfig():
     <html>
         <head> <title>ESP8266 Smart Config</title> </head>
         <body> <h1>ESP8266 Smart Config</h1>
-                <table border="1"> <tr><th>SSID</th><th>Value</th></tr> %s </table>
+                <table border="1"> <tr><th>SSID</th><th>channel</th><th>signal</th></tr> %s </table>
         </body>
     </html>
     """
@@ -27,7 +27,7 @@ def smartconfig():
             line = cl_file.readline()
             if not line or line == b'\r\n':
                 break
-        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (bytes.decode(i[0]), bytes.decode(i[1])) for i in ap_scan]
+        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (bytes.decode(i[0]), bytes.decode(i[2], bytes.decode(i[3])) for i in ap_scan]
         response = html % '\n'.join(rows)
         cl.send(response)
         cl.close()
